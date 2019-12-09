320a509a528f15bf85ef354b238c016369a0f72c
sirhypernova <sirhypernova+gh@gmail.com>
2019-12-09 10:57:59 -0600

    added disable hook to modules

--- modules/handler.js
+++ modules/handler.js
@@ -1,72 +1,83 @@
-const djmodule = require('./module');
-const path = require('path');
-const fs = require('fs');
-const { Collection } = require('discord.js');
+const djmodule = require("./module");
+const path = require("path");
+const fs = require("fs");
+const { Collection } = require("discord.js");
 
 class ModuleHandler {
-    constructor(dj) {
-        this.dj = dj;
-        this._modules = {};
-    }
-    
-    async add(modconf) {
-        var { name } = modconf;
-        var onLoad = modconf.onLoad || function () {};
-        var events = modconf.events || {};
-        var modifier = modconf.modifier || function (m) {return m};
-        
-        if (!this.exists(name)) return this._modules[name] = new djmodule({name, events, modifier, dj: this.dj});
-    }
-    
-    scan(directory) {
-        return new Promise((resolve,reject) => {
-            var dir = path.resolve(directory);
-            fs.stat(dir, (err) => {
-               if (err) return reject('Invalid Path');
-               fs.readdir(dir, (err,files) => {
-                   if (err) return reject(err);
-                   files.forEach(file => {
-                      if (!file.endsWith('.js')) return;
-                      var name = file.slice(0,-3);
-                      var module = require(path.resolve(dir,name));
-                      module.name = name;
-                      this.add(module);
-                   });
-                   resolve();
-               });
-            });
+  constructor(dj) {
+    this.dj = dj;
+    this._modules = {};
+  }
+
+  async add(modconf) {
+    var { name } = modconf;
+    var onLoad = modconf.onLoad || function() {};
+    var events = modconf.events || {};
+    var modifier =
+      modconf.modifier ||
+      function(m) {
+        return m;
+      };
+
+    if (!this.exists(name))
+      return (this._modules[name] = new djmodule({
+        name,
+        events,
+        modifier,
+        dj: this.dj
+      }));
+  }
+
+  scan(directory) {
+    return new Promise((resolve, reject) => {
+      var dir = path.resolve(directory);
+      fs.stat(dir, err => {
+        if (err) return reject("Invalid Path");
+        fs.readdir(dir, (err, files) => {
+          if (err) return reject(err);
+          files.forEach(file => {
+            if (!file.endsWith(".js")) return;
+            var name = file.slice(0, -3);
+            var module = require(path.resolve(dir, name));
+            module.name = name;
+            this.add(module);
+          });
+          resolve();
         });
+      });
+    });
+  }
+
+  get(module) {
+    return this._modules[module];
+  }
+
+  get collection() {
+    var array = [];
+    for (var key in this._modules) {
+      array.push([key, this._modules[key]]);
     }
-    
-    get(module) {
-        return this._modules[module];
+    return new Collection(array);
+  }
+
+  exists(module) {
+    return this._modules.hasOwnProperty(module);
+  }
+
+  ready() {
+    for (var module in this._modules) {
+      this._modules[module].emit("ready");
+      this._modules[module].firstLoad = false;
     }
-    
-    get collection() {
-        var array = [];
-        for (var key in this._modules) {
-            array.push([key,this._modules[key]]);
-        }
-        return new Collection(array);
-    }
-    
-    exists(module) {
-        return this._modules.hasOwnProperty(module);
-    }
-    
-    ready() {
-        for (var module in this._modules) {
-            this._modules[module].emit('ready');
-        }
-        this.dj.oldEmit = this.dj.emit;
-        this.dj.emit = (t,...d) => {
-            for (var module in this._modules) {
-                this._modules[module].emit('any',d,t);
-                this._modules[module].emit(t,...d);
-            }
-            this.dj.oldEmit(t,...d);
-        }
-    }
+    this.dj.oldEmit = this.dj.emit;
+    this.dj.emit = (t, ...d) => {
+      for (var module in this._modules) {
+        this._modules[module].emit("any", d, t);
+        this._modules[module].emit(t, ...d);
+      }
+      this.dj.oldEmit(t, ...d);
+    };
+  }
 }
 
 module.exports = ModuleHandler;
--- modules/module.js
+++ modules/module.js
@@ -1,41 +1,48 @@
-class Module extends require('events') {
-    constructor(data) {
-        super();
-        this.dj = data.dj;
-        this._name = data.name;
-        this._events = data.events;
-        this._loaded = true;
-        this._modifier = data.modifier;
-        this.data = {};
+class Module extends require("events") {
+  constructor(data) {
+    super();
+    this.dj = data.dj;
+    this._name = data.name;
+    this._events = data.events;
+    this._loaded = true;
+    this.firstLoad = true;
+    this._modifier = data.modifier;
+    this.data = {};
+  }
+
+  async disable() {
+    if (!this._loaded) return;
+    this.dj.commands.removeAllWhere(
+      command => command.module._name == this._name
+    );
+    if ("disable" in this._events) {
+      let res = this._events["disable"]();
+      if (res instanceof Promise) await res;
     }
-    
-    disable() {
-        if (!this._loaded) return;
-        this.dj.commands.removeAllWhere((command) => command.module._name == this._name);
-        this._eventsCache = this._events;
-        this._events = {};
-        this._loaded = false;
-    }
-    
-    enable() {
-        if (this._loaded) return;
-        this._events = this._eventsCache;
-        this._loaded = true;
-        this.emit('ready');
-    }
-    
-    addCommand(data) {
-        data.module = this;
-        return this.dj.commands.add(data);
-    }
-    
-    scanCommands(directory) {
-        return this.dj.commands.scan(directory,this);
-    }
-    
-    scanChecks(directory) {
-        return this.dj.checks.scan(directory,this._name);
-    }
+    this._eventsCache = this._events;
+    this._events = {};
+    this._loaded = false;
+  }
+
+  enable() {
+    if (this._loaded) return;
+    this._events = this._eventsCache;
+    this._loaded = true;
+    this.emit("ready");
+  }
+
+  addCommand(data) {
+    data.module = this;
+    return this.dj.commands.add(data);
+  }
+
+  scanCommands(directory) {
+    return this.dj.commands.scan(directory, this);
+  }
+
+  scanChecks(directory) {
+    return this.dj.checks.scan(directory, this._name);
+  }
 }
 
 module.exports = Module;
